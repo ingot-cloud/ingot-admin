2d31a377f2f2e93223b28346ca8f0de433f73931
jy <magician.of.technique@aliyun.com>
2026-09-13 11:02:47 +0800

    refactor: 提交 components.d.ts

--- apps/admin/components.d.ts
+++ apps/admin/components.d.ts
@@ -161,7 +161,4 @@ declare module 'vue' {
     VerifyPoints: typeof import('./../../packages/admin-core/src/components/verifition/verify/VerifyPoints.vue')['default']
     VerifySlide: typeof import('./../../packages/admin-core/src/components/verifition/verify/VerifySlide.vue')['default']
   }
-  export interface GlobalDirectives {
-    vLoading: typeof import('element-plus/es')['ElLoadingDirective']
-  }
 }
